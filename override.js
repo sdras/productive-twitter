34b40f994435d8d86ff559b89f3e310cb346dd3f
Sarah Drasner <sarah.drasner@gmail.com>
2019-07-20 00:17:55 -0600

    work on middle

--- override.js
+++ override.js
@@ -1,6 +1,10 @@
 setTimeout(() => {
   var sidebaritem = document.querySelector("aside")
-  //var sidebarcontain = sidebaritem.parentNode.parentNode
+  var sidebarcontain = sidebaritem.parentNode.parentNode.parentNode
+  var children = sidebarcontain.childNodes
 
-  console.log(sidebaritem)
+  console.log(Array.isArray(children))
+  // var finalchildren = children.shift()
+
+  // finalchildren.style.display = "none"
 }, 1000)
