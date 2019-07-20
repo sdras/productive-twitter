1b259609c9f82d2c6f650e32bc7f798df4f688cd
Sarah Drasner <sarah.drasner@gmail.com>
2019-07-19 23:28:22 -0600

    add in fonts

--- override.js
+++ override.js
@@ -1,1 +1,4 @@
-console.log("working")
+// const sidebaritem = document.querySelector('aside[aria-label="Who to follow"]')
+// const sidebarcontain = sidebaritem.parentNode.parentNode
+
+// console.log(sidebarcontain)
