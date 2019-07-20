de7ad2c530b6dd3eb9bb8c34b7b8d9e79c43ce7b
Sarah Drasner <sarah.drasner@gmail.com>
2019-07-20 01:22:13 -0600

    keep working on middle style tweaks

--- override.js
+++ override.js
@@ -1,9 +1,16 @@
-setTimeout(() => {
-  var sidebaritem = document.querySelector("aside")
-  var sidebarcontain = sidebaritem.parentNode.parentNode.parentNode
-  var children = sidebarcontain.childNodes
+function replaceSidebar() {
+  setTimeout(() => {
+    const sidebaritem = document.querySelector("aside"),
+      sidebarcontain = sidebaritem.parentNode.parentNode.parentNode,
+      children = sidebarcontain.childNodes
 
-  children.forEach((el, i) => {
-    i === 0 ? (el.style.marginTop = "10px") : (el.style.display = "none")
-  })
-}, 1000)
+    children.forEach((el, i) => {
+      i === 0 ? (el.style.marginTop = "5px") : (el.style.display = "none")
+    })
+  }, 1000)
+}
+
+replaceSidebar()
+if ("onhashchange" in window) {
+  replaceSidebar()
+}
