ac018b3538f9f2ef895a2898446a109e33f4062b
Sarah Drasner <sarah.drasner@gmail.com>
2020-07-18 18:00:27 -0600

    make it work on all color themes

--- override.js
+++ override.js
@@ -16,3 +16,10 @@ console.log(
 `,
   "font-family:monospace"
 )
+
+const body = document.querySelector("body")
+
+if (body.style.backgroundColor === "rgb(21, 32, 43)") {
+  console.log("hi")
+  body.classList.add("dim")
+}
